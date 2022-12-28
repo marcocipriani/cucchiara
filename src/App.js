5d63482ffebf5221edfcf2a43ef79537c37500a8
marcocipriani <marcocipriani@users.noreply.github.com>
2022-12-28 15:03:18 +0100

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>A cucchiara de legno</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
