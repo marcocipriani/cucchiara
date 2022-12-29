78965957fad3457bb4739b86bd5c35ea34ef4af3
marcocipriani <marcocipriani@users.noreply.github.com>
2022-12-29 13:09:40 +0100

    set react-router up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router";
+import { Link } from "react-router-dom";
+
+// pages
+import Home from "./pages/home.pages";
+import About from "./pages/about.pages";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>A cucchiara de legno</h1>
+				<nav>
+					<Link to="/">Home</Link>
+					<Link to="/about">About</Link>
+				</nav>
 			</header>
+			<Routes>
+				<Route index element={<Home />}></Route>
+				<Route path="/about" element={<About />}></Route>
+			</Routes>
 		</div>
 	);
 }
---
+++ src/pages/home.pages.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+	return (
+		<div className="home">
+			<h2>Home</h2>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Quisquam a obcaecati velit praesentium, quod minus eius porro
+				quasi iste sit labore aperiam inventore, numquam temporibus
+				iusto recusandae dolores distinctio? Autem.
+			</p>
+		</div>
+	);
+}
